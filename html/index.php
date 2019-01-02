6370ccf08fbdad5fc65bd588556134e772fe64cc
ChristianAlbrechtCAl <43741524+ChristianAlbrechtCAl@users.noreply.github.com>
2019-01-02 20:19:23 +0200

    Add initial website files

---
+++ html/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>MakerSpace</title>
+    <link rel="stylesheet" type="text/css" href="../css/index.css">
+    <link rel="icon" href="../sources/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Charm|Open+Sans" rel="stylesheet">
+  </head>
+  <body>
+
+      <div class="introBox">
+        <h1 style="text-decoration: none;">Hei there!</h1>
+        <p>My name is <span class="goal">Christian</span>.</p>
+        <div class="mountain">
+          <img src="../sources/mountain.png" alt="mountain" />
+        </div>
+      </div>
+
+      <img class="firstCloud" src="../sources/cloud.png"/>
+      <img class="secondCloud" src="../sources/cloud.png"/>
+      <img class="thirdCloud" src="../sources/cloud.png"/>
+      <img class="fourthCloud" src="../sources/cloud.png"/>
+      <img class="fifthCloud" src="../sources/cloud.png"/>
+
+      <div class="personalIntroduction">
+        <img src="../sources/angela.png" />
+        <p>I am a software developer with the dream to develop his own social network.</p>
+      </div>
+
+      <br />
+      <hr />
+      <br />
+      <div class="skillsSection">
+        <h1>My skills</h1>
+        <br />
+        <img src="../sources/coding.png" />
+        <p>I am an experienced software developer who has already published several projects.</p>
+        <br />
+        <br />
+        <img src="../sources/ping-pong.png"  />
+        <p>I used to be a professional table tennis player in my childhood.</p>
+      </div>
+
+      <br />
+      <hr />
+      <br />
+
+      <div class="contactSection">
+        <h1>Contact</h1>
+        <br />
+
+        <a href="mailto:chrisalbrecht17@googlemail.com">Contact me</a>
+        <br />
+        <a class="socialNetworkContact" href="https://www.instagram.com/a_slide_of_improvement/?hl=de">Instagram</a>
+        <a class="socialNetworkContact" href="https://www.facebook.com/checkpoint/?next=https%3A%2F%2Fwww.facebook.com%2Fgettingstarted%2F%3Fstep%3Dcontact_importer">Facebook</a>
+        <a class="socialNetworkContact" href="https://www.linkedin.com/uas/login">LinkedIn</a>
+      </div>
+
+      <br />
+      <br />
+
+  </body>
+</html>
